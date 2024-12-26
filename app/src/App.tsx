2789610c4b8845fa947ec1954c5fa84175a57bd7
matteussaraujo <mateusdev36@gmail.com>
2024-12-26 01:48:39 -0300

    Logo adicionada

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    
+      <div className="container h-screen w-screen mx-auto flex items-center justify-center">
+        <div className="flex flex-row items-center">
+        <img className="w-24 rounded-full " src="https://github.com/matteussaraujo.png" alt="User Avatar" />
+        </div>
+        </div>
+
+    
+  )
+}
+
+export default App
